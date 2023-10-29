25681ee65a47c4d1a5271ed86861080401bb8ac0
Elnur Seyidov <55358917+elnurseyidovv@users.noreply.github.com>
2023-10-29 22:54:42 +0100

    Added documentation to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,78 @@
+// External dependencies
 use std::error::Error;
 use rand::rngs::OsRng;
 use ed25519_dalek::{Signer, SigningKey, VerifyingKey};
-mod mimc;
-mod builder;
-
 use std::io;
 use std::io::Write;
 use ark_bn254::Bn254;
 use num_bigint;
-
 use ark_std::rand::thread_rng;
 use color_eyre::Result;
-
 use ark_crypto_primitives::snark::SNARK;
 use ark_ff::BigInt;
 use ark_groth16::Groth16;
 use ed25519_compact::{KeyPair, Seed};
 use ed25519_dalek::ed25519::signature::rand_core::RngCore;
+
+// Internal modules
+mod mimc;
+mod builder;
 use crate::builder::CircomConfig;
 
+// Type definition for Groth16 using Bn254 curve
 type GrothBn = Groth16<Bn254>;
 
+// Initial balances for sender and receiver
 const SENDER_ETH: u32 = 500;
 const RECEIVER_ETH: u32 = 0;
 
 fn main() -> Result<(), Box<dyn Error>> {
-
+    // Generating a default key pair
     let key_pair = KeyPair::from_seed(Seed::default());
 
-
-    // Generate a sender and receiver keypair
+    // Generating sender and receiver key pairs using RNG
     let mut csprng1 = OsRng;
     let sender_key = SigningKey::generate(&mut csprng1);
 
     let mut csprng2 = OsRng;
     let receiver_key = SigningKey::generate(&mut csprng2);
 
+    // Printing sender public key
     println!("Sender public key: {:?}", convert_bytes(sender_key.verifying_key().to_bytes()).to_string());
 
-    // Ask for the amount of ETH for the transaction
+    // Display sender and receiver initial balances and get transaction amount from user
     print!("Sender ETH: {}\n", SENDER_ETH);
     print!("Receiver ETH: {}\n", RECEIVER_ETH);
     print!("Enter the amount of ETH for the transaction: ");
     io::stdout().flush()?;  // Ensure the print! output is displayed immediately
     let send_eth_amount = read_u64_input()?;
 
-    // Hash the sender and receiver accounts and calculate the accounts root
+    // Hash sender and receiver accounts and calculate the accounts root
     let sender_hash = hash_account(&sender_key.verifying_key(), SENDER_ETH)?;
     let receiver_hash = hash_account(&receiver_key.verifying_key(), RECEIVER_ETH)?;
     let accounts_root = root_hash(&sender_hash, &receiver_hash)?;
 
-    // Hash the transaction, sign it and verify the signature
+    // Hash the transaction, sign it, and verify the signature
     let transaction_arr = hash_transaction(&sender_key.verifying_key(), &receiver_key.verifying_key(), send_eth_amount)?;
     let signature = sender_key.sign(&transaction_arr.to_signed_bytes_be());
     let verified = sender_key.verify(&transaction_arr.to_signed_bytes_be(), &signature);
     println!("Signature verified: {:?}", verified.is_ok());
 
-    // Calculate the new account hashes after the transaction
+    // Calculate new account hashes post-transaction
     let new_sender_hash = hash_account(&sender_key.verifying_key(), SENDER_ETH - send_eth_amount)?;
     let new_receiver_hash = hash_account(&receiver_key.verifying_key(), RECEIVER_ETH + send_eth_amount)?;
 
-    // Calculate the intermediate and new roots
+    // Calculate intermediate and new roots post-transaction
     let intermediate_accounts_root = root_hash(&new_sender_hash, &receiver_hash)?;
     let new_accounts_root = root_hash(&new_sender_hash, &new_receiver_hash)?;
 
-    // Load the WASM and R1CS for witness and proof generation
+    // Load WASM and R1CS files for witness and proof generation
     let cfg = CircomConfig::<Bn254>::new(
         "./circuits/circuit_js/circuit.wasm",
         "./circuits/circuit.r1cs",
     )?;
 
-    // Insert our public inputs as key value pairs
+    // Populate zkSNARK circuit with public inputs
     let mut builder = builder::CircomBuilder::new(cfg);
     builder.push_input("accounts_root", accounts_root);
     builder.push_input("intermediate_root", intermediate_accounts_root);
@@ -80,7 +82,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     builder.push_input("receiver_balance", num_bigint::BigInt::from(RECEIVER_ETH));
     builder.push_input("amount", num_bigint::BigInt::from(send_eth_amount));
     builder.push_input("signature_R8x", signature.r_bytes());
-    // builder.push_input("signature_S", signature.s_bytes().into());
 
 
     let mut sender_proof = Vec::new();
@@ -100,22 +101,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     builder.push_input_vec("receiver_proof_pos", receiver_proof_pos);
     builder.push_input("enabled", 1);
 
-    // Create an empty instance for setting it up
+    // Create empty instance
     let circom = builder.setup();
 
-    // Run a trusted setup
+    // Setup using Groth16
     let mut rng = thread_rng();
     let params = GrothBn::generate_random_parameters_with_reduction(circom, &mut rng)?;
 
-    // Get the populated instance of the circuit with the witness
+    // Add witness to the circuit 
     let circom = builder.build()?;
 
-    let inputs = circom.get_public_inputs().unwrap();
-
-    // Generate the proof
+    // Generate the zero-knowledge proof
     let proof = GrothBn::prove(&params, circom, &mut rng)?;
 
-    // Check that the proof is valid
+    // Verify the generated proof
     let pvk = GrothBn::process_vk(&params.vk)?;
     let verified = GrothBn::verify_with_processed_vk(&pvk, &inputs, &proof)?;
     assert!(verified);
@@ -123,6 +122,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+// Reads an input value (u32) from the console.
 fn read_u64_input() -> Result<u32, Box<dyn Error>> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
@@ -130,6 +130,7 @@ fn read_u64_input() -> Result<u32, Box<dyn Error>> {
     Ok(parsed_input)
 }
 
+// Returns the MiMC hash of an account given its verifying key and balance.
 fn hash_account(key: &VerifyingKey, balance: u32) -> Result<num_bigint::BigInt, Box<dyn Error>> {
     let mut hasher = mimc::Mimc7::new();
     let mut account_vec: Vec<num_bigint::BigInt> = Vec::new();
@@ -138,12 +139,14 @@ fn hash_account(key: &VerifyingKey, balance: u32) -> Result<num_bigint::BigInt, 
     Ok(hasher.hash(account_vec)?)
 }
 
+// Returns the MiMC hash of two combined hashes.
 fn root_hash(hash1: &num_bigint::BigInt, hash2: &num_bigint::BigInt) -> Result<num_bigint::BigInt, Box<dyn Error>> {
     let mut hasher = mimc::Mimc7::new();
     let account_hashes = vec![hash1.clone(), hash2.clone()];
     Ok(hasher.hash(account_hashes)?)
 }
 
+// Hashes the details of a transaction using the MiMC hash function.
 fn hash_transaction(sender_key: &VerifyingKey, receiver_key: &VerifyingKey, amount: u32) -> Result<num_bigint::BigInt, Box<dyn Error>> {
     let mut hasher = mimc::Mimc7::new();
     let mut transaction_vec = Vec::new();
